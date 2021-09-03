10bff37912b4835118f553693778d8ac6a1c427c
Shane0901 <shanemagorrian09@gmail.com>
2021-09-03 18:11:42 +0100

    update_06
    Update 6. images and layout updated. Old code removed

--- contactform.php
+++ contactform.php
@@ -1,16 +1,35 @@
- <?php
 
+<?php
 
- if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject =$_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
+// define variables and set to empty values
+$nameErr = $emailErr = "";
+$name = $email =  $comment = "";
 
-    $mailTo = "shanemagorrian09@outlook.ie";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have recieved an e-mail from ".$name."\n\n".$message;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
-     }
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+}
+?>
